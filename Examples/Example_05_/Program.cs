854e29f22f5f6e1cf06b4985d0518f76f813ca76
AMORE76 <gemenius007@internet.ru>
2022-08-08 20:25:14 +0300

    Add folder Example_05_

---
+++ Examples/Example_05_/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Input username. My name is ");
+string username = Console.ReadLine();
+
+/* Console.Write("Hello, ");
+Console.Write(username!); */
+
+if(username.ToLower() == "mike")
+{
+   Console.Write("Hurray, it is ");
+} 
+else
+{
+   Console.Write("Hello, ");
+}
+
+Console.Write(username);
+Console.Write("!");
