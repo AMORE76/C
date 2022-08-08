9264188b9fee2df62fefe816f897c6d00f9fb7eb
AMORE76 <gemenius007@internet.ru>
2022-08-08 18:57:51 +0300

    Add random in folder Example_03_Sum

--- Examples/Example_03_Sum/Program.cs
+++ Examples/Example_03_Sum/Program.cs
@@ -6,3 +6,9 @@ int numberC = 3;
 int numberD = 2;
 int result = numberC + numberD;
 Console.WriteLine(result);
+
+int numberE = new Random().Next(1,10);
+Console.WriteLine(numberE);
+int numberF = new Random().Next(1,10);
+Console.WriteLine(numberF);
+Console.WriteLine(numberE + numberF);
