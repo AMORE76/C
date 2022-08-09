e2d75a1aabf13389fffbf79426390490c58db3b9
AMORE76 <gemenius007@internet.ru>
2022-08-09 19:14:53 +0300

    Make a desicion of HomeTask_001_02

--- Hometasks/HomeTask_001_02/Program.cs
+++ Hometasks/HomeTask_001_02/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 4: Напишите программу, 
+которая принимает на вход три числа и 
+выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+/*int a1 = 2;
+int b1 = 3;
+int c1 = 7;
+
+{int inter_max = a1;
+
+int result1 = a1 - b1;
+
+if (result1 > 0) inter_max = a1;
+if (result1 < 0) inter_max = b1;
+
+int final_max = inter_max;
+
+int result2 = c1 - inter_max;
+
+if (result2 > 0) final_max = c1;
+if (result2 < 0) final_max = inter_max;
+
+Console.Write("final_max = ");
+Console.WriteLine(final_max);} */
+
+/* int a1 = 44;
+int b1 = 5;
+int c1 = 78;
+
+{int inter_max = a1;
+
+int result1 = a1 - b1;
+
+if (result1 > 0) inter_max = a1;
+if (result1 < 0) inter_max = b1;
+
+int final_max = inter_max;
+
+int result2 = c1 - inter_max;
+
+if (result2 > 0) final_max = c1;
+if (result2 < 0) final_max = inter_max;
+
+Console.Write("final_max = ");
+Console.WriteLine(final_max);} */
+
+int a1 = 22;
+int b1 = 3;
+int c1 = 9;
+
+{int inter_max = a1;
+
+int result1 = a1 - b1;
+
+if (result1 > 0) inter_max = a1;
+if (result1 < 0) inter_max = b1;
+
+int final_max = inter_max;
+
+int result2 = c1 - inter_max;
+
+if (result2 > 0) final_max = c1;
+if (result2 < 0) final_max = inter_max;
+
+Console.Write("final_max = ");
+Console.WriteLine(final_max);}
+
+
+
+
