895db9738b45798b194a1bc4bd897044a170fda6
AMORE76 <gemenius007@internet.ru>
2022-08-09 12:38:10 +0300

    Add folder Example_07_3Points

---
+++ Examples/Example_07_3Points/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int 
+xa = 24, ya = 1,
+xb = 1, yb = 12,
+xc = 48, yc = 12;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
