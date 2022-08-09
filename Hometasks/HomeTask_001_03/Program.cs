8e0348ff4d04c9451ff880fe4ee317234a0221cc
AMORE76 <gemenius007@internet.ru>
2022-08-09 21:15:09 +0300

    Add folder Hometask_001_03 and make its decision

---
+++ Hometasks/HomeTask_001_03/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 6: Напишите программу, 
+которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет */
+
+
+/* 
+{
+    int a1 = 4;
+int result1 = (a1 % 2);
+
+
+{
+    if (result1 == 0)
+
+    { Console.WriteLine("Var a1 is an even number"); }
+
+    else
+
+    { Console.WriteLine("Var a2 is an odd number"); 
+};
+*/
+
+/* int a2 = -3;
+int result2 = (a2 % 2);
+
+{
+    if (result2 == 0)
+
+    { Console.WriteLine("Var a2 is an even number"); }
+
+    else
+
+    { Console.WriteLine("Var a2 is an odd number"); }
+}
+*/
+
+int a3 = 7;
+int result3 = (a3 % 2);
+
+{
+    if (result3 == 0)
+
+    { Console.WriteLine("Var a3 is an even number"); }
+
+    else
+
+    { Console.WriteLine("Var a3 is an odd number"); }
+}
